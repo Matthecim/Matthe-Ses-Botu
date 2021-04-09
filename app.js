2effff839e7b070ec294c386f2905f57bab1c89c
Matthecim <78787135+Matthecim@users.noreply.github.com>
2021-04-09 14:56:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ Voice.on("voiceStateUpdate", async(oldState, newState) => {
     }
 });
 
-Voice.login(lozayar.TOKEN).catch(err => {
+Voice.login(process.env.token).catch(err => {
     Voice.error("Ses istemcisine bağlanırken bir hata oluştu: " + err.message);
     return Voice.destroy();
 });
@@ -128,4 +128,6 @@ GuildMember.prototype.isStaff = function() {
     ) return true;
     return false;
 }
+
+
 
